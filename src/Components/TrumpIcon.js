a085ccad06781dc23ad91de1cca177115e27d5f6
Giannoulo <ggiannoulopoulos@konnektable.com>
2021-09-06 01:55:49 +0300

    Added hit sound

--- src/Components/TrumpIcon.js
+++ src/Components/TrumpIcon.js
@@ -1,12 +1,23 @@
 import React from "react";
 import TrumpSvg from "../Assets//trump.svg";
 import TrumpSvg2 from "../Assets/trumpIcon2.svg";
+import trumpHit from "../Assets/10-BILLION.mp3";
 
 const TrumpIcon = (props) => {
+  const playAudio = () => {
+    new Audio(trumpHit).play();
+  };
   return (
     <div>
       <img
-        onClick={props.clickCallback === null ? null : () => props.clickCallback()}
+        onClick={
+          props.clickCallback === null
+            ? () => playAudio()
+            : () => {
+                props.clickCallback();
+                playAudio();
+              }
+        }
         src={TrumpSvg2}
         alt="Trump icon"
         draggable="false"
