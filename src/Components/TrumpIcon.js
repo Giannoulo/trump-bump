f9198108af1db0d1ddaf742419d538345da56a86
Giannoulo <ggiannoulopoulos@konnektable.com>
2021-09-05 21:20:44 +0300

    Changed svg icon

--- src/Components/TrumpIcon.js
+++ src/Components/TrumpIcon.js
@@ -1,12 +1,13 @@
 import React from "react";
 import TrumpSvg from "../Assets//trump.svg";
+import TrumpSvg2 from "../Assets/trumpIcon2.svg";
 
 const TrumpIcon = (props) => {
   return (
     <div>
       <img
         onClick={props.clickCallback === null ? null : () => props.clickCallback()}
-        src={TrumpSvg}
+        src={TrumpSvg2}
         alt="Trump icon"
         draggable="false"
         className={`trump-svg ${props.classNameProp}`}
