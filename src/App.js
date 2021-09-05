8e52d19a6640c1fde5bc17ee3f6e05e659427097
Giannoulo <ggiannoulopoulos@konnektable.com>
2021-09-06 01:41:18 +0300

    Added test hammer cursors

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Components/Header";
 import Controls from "./Components/Controls";
 import GameBoard from "./Components/GameBoard";
 
 const App = () => {
+  const [cursor, setCursor] = useState("unclicked");
+
+  const changeCursor = () => {
+    setCursor("clicked");
+  };
+
+  useEffect(() => {
+    if (cursor === "clicked") {
+      setTimeout(() => {
+        setCursor("unclicked");
+      }, 90);
+    }
+  }, [cursor]);
+
   return (
-    <div className="App">
+    <div className={`App ${cursor}`} onClick={() => changeCursor()}>
       <Header />
       <Controls />
       <GameBoard />
